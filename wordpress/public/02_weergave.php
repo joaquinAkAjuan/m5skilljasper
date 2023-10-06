65aeccb91daf6c7346d5ff652ecdd28c520a6567
joaquinAkAjuan <112825010+joaquinAkAjuan@users.noreply.github.com>
2023-10-06 13:59:38 +0200

    wordpress

---
+++ wordpress/public/02_weergave.php
@@ -0,0 +1,22 @@
+<?php
+// laat alle errors zien:
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+?>
+<?php
+$voornaam = "Joaquin";
+$achternaam = "de Boer";
+$string = "ik ben";
+
+echo "$string $voornaam $achternaam"
+?>
+<h1>Maak een samenstelling van 2 variabelen én 1 string</h1>
+De variabele 1 bevat een voornaam<br>
+De variabele 2 bevat een achternaam<br>
+<br>
+<h3>Resultaat:</h3>
+Mijn naam is Jasper Heidebrink<br>
+<br>
+<br>
+<b>Extra:</b>
+Maak 3 verschillende codes die in alle gevallen hetzelfde resultaat geven in HTML.
